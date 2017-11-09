202d80a77c3d76dff8a17708875d5b881e924964
Ashley Cummins <acummin1@nd.edu>
2017-11-09 16:24:10 -0500

    create shop screen

---
+++ Game/Assets/Shop/Back.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Back : MonoBehaviour {
+
+	public void go_back(string s){
+		Application.LoadLevel (s);
+	}
+}
