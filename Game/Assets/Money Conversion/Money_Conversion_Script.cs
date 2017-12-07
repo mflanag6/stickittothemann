7564abc92435df5ae0adc9fdab1c8dfb46d313ab
Ashley Cummins <acummin1@nd.edu>
2017-12-07 15:53:04 -0500

    merging

---
+++ Game/Assets/Money Conversion/Money_Conversion_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money_Conversion_Script : MonoBehaviour {
+
+	// How to play popup
+	public GameObject HowToPlay_Popup;
+
+	// Use this for initialization
+	void Start () {
+		// make sure how to play popup isn't showing
+		HowToPlay_Popup.SetActive(false);
+	}
+
+	// How to Play button
+	public void OnHowToButtonClick(){
+		// Open up how to play popup
+		HowToPlay_Popup.SetActive(true);
+	}
+}
