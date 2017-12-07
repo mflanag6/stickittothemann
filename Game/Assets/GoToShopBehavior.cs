e795658c9d9b4a2eba3194559af00ef6c0ee6749
Ashley Cummins <acummin1@nd.edu>
2017-12-07 16:11:18 -0500

    I is adding a button boi

--- Game/Assets/GoToShopBehavior.cs
+++ Game/Assets/GoToShopBehavior.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class GoToShopBehavior : MonoBehaviour {
 
+	public GameObject Game_UI;
+
 	public void go_to_shop(string s){
 		// Load scene s
 		Application.LoadLevel(s);
+		// Disable viewing all other things
+		Game_UI.SetActive (false); 
 	}
 }
