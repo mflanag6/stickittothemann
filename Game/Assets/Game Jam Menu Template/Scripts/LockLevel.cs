903ddec3a747d51852ce60b65fb659ead47c89a0
Nick Palutsis <npalutsi@nd.edu>
2017-12-07 16:03:13 -0500

    added editted music and graphics

---
+++ Game/Assets/Game Jam Menu Template/Scripts/LockLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    GameObject go = GameObject.Find("VisualHealth");
+ 
+    //check if we have a match 
+        if(go!= null) {
+        //assign sprite reference to your variable 
+        _canvas = go.GetComponent<SpriteRenderer>().sprite;
+    }
+}
