6aedc5f35ff34b36ff517081e19a0ed7ce628ea9
Ashley Cummins <acummin1@nd.edu>
2017-12-07 16:08:20 -0500

    added go to shop button

---
+++ Game/Assets/GoToShopBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToShopBehavior : MonoBehaviour {
+
+	public void go_to_shop(string s){
+		// Load scene s
+		Application.LoadLevel(s);
+	}
+}
