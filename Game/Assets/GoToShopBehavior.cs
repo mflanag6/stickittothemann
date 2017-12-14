34b67070bc879c4a1dcd7446bf76abbec71df315
Matthew Flanagan <matthew.flanagan15@gmail.com>
2017-12-14 10:15:06 -0500

    Persistence through shop

--- Game/Assets/GoToShopBehavior.cs
+++ Game/Assets/GoToShopBehavior.cs
@@ -12,14 +12,11 @@ public class GoToShopBehavior : MonoBehaviour {
 	public bool created = false;
 
 	public void Start(){
-
 		// Begin with Shop button disabled 
-		//shop_button.enabled = false;
 		shop_button.SetActive(false);
 	}
 
 	public void Show_Shop_Button(){
-		//shop_button.enabled = true;
 		shop_button.SetActive(true);
 	}
 
@@ -27,17 +24,7 @@ public class GoToShopBehavior : MonoBehaviour {
 	public void go_to_shop(string s){
 		// Load scene s
 		Application.LoadLevel(s);
-		//SceneManager.Set
-		//SceneManager.LoadScene(s, LoadSceneMode.Additive);
-		// Disable viewing all other things
-		Game_UI.SetActive(false);	
 	}	
 
-	public void back(){
-		Debug.LogWarning ("back!@!@!#");
-		//Game_UI.SetActive(true);
-		// Load scene s
-		//Application.LoadLevel (s);
-	}
 }
  
--- Game/Assets/Shop/Scripts/Back.cs
+++ Game/Assets/Shop/Scripts/Back.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Back : GoToShopBehavior {
+public class Back : MonoBehaviour {
+
+	public GameObject Game_UI;
+
+	public void Awake() {
+		Game_UI = GameObject.FindWithTag("UI");
+		Game_UI.SetActive (false);
+	}
 
 	public void go_back(string s){
-		Game_UI = GameObject.FindWithTag("UI");
-		Game_UI.SetActive (true);
+		Game_UI.SetActive(true);
 		// Load scene s
 		Application.LoadLevel (s);
 	}
