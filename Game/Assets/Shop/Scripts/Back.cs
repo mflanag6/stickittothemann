785dd1a71139f449dda04fb809b201c29aa3c753
Matthew Flanagan <matthew.flanagan15@gmail.com>
2017-12-14 14:49:14 -0500

    Hide gameboard on shop

--- Game/Assets/Shop/Scripts/Back.cs
+++ Game/Assets/Shop/Scripts/Back.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Back : MonoBehaviour {
 
 	public GameObject Game_UI;
+	public GameObject Board_UI;
 
 	public void Awake() {
 		Game_UI = GameObject.FindWithTag("UI");
-		Game_UI.SetActive (false);
+		Board_UI = GameObject.FindWithTag("Multiply");
+		Game_UI.SetActive(false);
+		Board_UI.SetActive (false);
 	}
 
 	public void go_back(string s){
 		Game_UI.SetActive(true);
+		Board_UI.SetActive (true);
 		// Load scene s
 		Application.LoadLevel (s);
 	}
